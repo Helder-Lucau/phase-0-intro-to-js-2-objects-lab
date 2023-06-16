25feeecdc1ec1fe1e28578b89eea2edbe93bab11
Helder <helderlucau@gmail.com>
2023-06-16 16:52:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {
+    name: "Asensio",
+    streetAddress: "11 Broadway"
+};
+employee;
+
+function updateEmployeeWithKeyAndValue(){
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+
+}
+
+function deleteFromEmployeeByKey(){
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+    
+}
